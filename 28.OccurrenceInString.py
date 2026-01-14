d107428fffdee3a2b0fd5526565f0612798a5bd0
Areeb <areebamir1120@gmail.com>
2026-01-13 20:29:24 -0500

    Solved 28 problem; string proble

---
+++ 28.OccurrenceInString.py
@@ -0,0 +1,13 @@
+
+# 28. Find the Index of the First Occurrence in a String
+
+haystack = "sadbutsad"
+needle = "sad"
+
+if needle in haystack:
+    index = haystack.find(needle)
+    print(index)
+else:
+    print(-1)
+
+
